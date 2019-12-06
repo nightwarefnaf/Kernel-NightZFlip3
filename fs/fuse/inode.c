08b1081187fb816e1ff1ea23414cbb4f1b116d2d
Eric Sandeen <sandeen@sandeen.net>
2019-12-06 10:45:01 -0600

    BACKPORT: fs_parser: remove fs_parameter_description name field
    Unused now.
    
    Signed-off-by: Eric Sandeen <sandeen@redhat.com>
    Acked-by: David Howells <dhowells@redhat.com>
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: tanz3 <3222839916@qq.com>

--- fs/fuse/inode.c
+++ fs/fuse/inode.c
@@ -481,7 +481,6 @@ static const struct fs_parameter_spec fuse_param_specs[] = {
 };
 
 static const struct fs_parameter_description fuse_fs_parameters = {
-	.name		= "fuse",
 	.specs		= fuse_param_specs,
 };
 
