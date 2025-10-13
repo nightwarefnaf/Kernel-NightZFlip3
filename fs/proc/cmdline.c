af287f7d6da01c05e5116d2d60618edfdc1279f9
AlexFurina <92114905+AlexFurina@users.noreply.github.com>
2025-10-13 17:35:49 +0900

    fs: proc: spoof vbmeta.device_state

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -96,6 +96,7 @@ static int __init proc_cmdline_init(void)
 	process_flag(FLAG_REPLACE, "androidboot.verifiedbootstate=", "green"); // Play Integrity API / SafetyNet
 	process_flag(FLAG_REPLACE, "androidboot.warranty_bit=", "0"); // Bootloader status and Knox
 	process_flag(FLAG_REPLACE, "androidboot.fmp_config=", "1"); // Samsung Knox FMP / FIPS
+	process_flag(FLAG_REPLACE, "androidboot.vbmeta.device_state=", "locked");
 #endif
 
 	proc_create_single("cmdline", 0, NULL, cmdline_proc_show);
